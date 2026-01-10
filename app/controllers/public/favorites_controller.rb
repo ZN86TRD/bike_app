754514af8e5ee3f3eaba1c699493b4552cff78ca
Ubuntu <ubuntu@ip-172-31-28-47.ap-northeast-1.compute.internal>
2026-01-10 11:46:35 +0000

    いいね機能の実装

---
+++ app/controllers/public/favorites_controller.rb
@@ -0,0 +1,23 @@
+class Public::FavoritesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
+    favorite.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: @post.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  def index
+    favorite_post_ids = current_user.favorites.pluck(:post_id)
+    @favorite_posts = Post.where(id: favorite_post_ids)
+  end
+end
