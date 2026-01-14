944b1a0bbc37fba5c8ead362da4c7f3e0d694466
Ubuntu <ubuntu@ip-172-31-28-47.ap-northeast-1.compute.internal>
2026-01-14 02:21:01 +0000

    いいね、コメントの非同期通信化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -6,14 +6,12 @@ class Public::FavoritesController < ApplicationController
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: @post.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 
   def index
